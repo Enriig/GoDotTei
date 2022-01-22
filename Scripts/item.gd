82225bf1f3368166b88cd4f7eb55f97985038e23
Henrique <hiqvieira@gmail.com>
2022-01-22 11:32:38 -0300

    Lista 12

---
+++ Scripts/item.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+
+func _on_item_body_entered(body):
+	print(body.name)
+	$anim.play("collected")
+
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "collected":
+		queue_free()
